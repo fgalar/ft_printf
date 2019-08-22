0d79d3d0c6cf0de4c8835b24550f8a4bd1807d91
fgalar.home <test@youremail.com>
2019-08-23 00:57:33 +0200

    mantissa ok... voir pour chiffres negatifs

--- print_f.c
+++ print_f.c
@@ -6,7 +6,7 @@
 /*   By: fanny <fgarault@student.42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/31 16:15:40 by fanny             #+#    #+#             */
-/*   Updated: 2019/08/23 00:39:58 by fanny            ###   ########.fr       */
+/*   Updated: 2019/08/23 00:56:01 by fanny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,22 +33,22 @@ int		ft_log(double value)
 char	*get_mantissa(double nb, char *tab)
 {
 	double		facto;
-	char		base[2] = {'0', '1'};
 	int			i;
-	double		f_loop;
 	
 	tab = (char*)malloc(sizeof(char) * (sizeof(double) + 1));
 	facto = (nb / ft_power(2, ft_log(nb)) -1); //	calcul du mantisse
 	i = 0;
-	while (facto != 0)
+	while (facto != 0 && i < 23)
 	{
 		facto += facto;
-		
-		printf("facto = %f\n", facto);
-		tab[i] = base[(int)facto];
-		i++;
 		if (facto > 1)
+		{
+			tab[i] = '1';
 			facto -= 1;
+		}
+		else
+			tab[i] = '0';
+		i++;
 	}
 	return(tab);
 }
@@ -84,7 +84,7 @@ int main(int ac, char **av)
 
 	//printf("Power = %d\n", ft_log(atoi(av[1])));
 	
-	ft_printf("%f\n", 263.3);
-	printf("%f\n", 263.3);
+	ft_printf("%f\n", 89.62);
+	printf("%f\n", 89.62);
 	return (0);
 }
